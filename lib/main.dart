d8177de41305d0cc37ad900ff854a6f6c59ef362
dpgehani20 <69641659+dpgehani20@users.noreply.github.com>
2021-01-30 09:10:24 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 15,
+      // backgroundColor: Colors.pinkAccent,
+      image : Image.asset('assets/liquid_preloader_by_volorf.gif'),
+      loaderColor: Colors.white,
+      photoSize: 150.0,
+      navigateAfterSeconds: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Welcome Screen'),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
